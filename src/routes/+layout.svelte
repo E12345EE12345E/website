85a09ccbdaad7b28273845810740ba64cf0bb32d
E12345EE12345E <60201680+E12345EE12345E@users.noreply.github.com>
2025-06-08 17:07:39 -0700

    page layout and canvas

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="navbar" id="header">
+    <div class="navbuttons">
+        <a href="./" class="navbutton">Home</a>
+        <a href="./" class="navbutton">Projects</a>
+        <a href="./" class="navbutton">Links</a>
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        background-color: black;
+        color: white;
+        font-family: "Lucida Console", monospace;
+        box-sizing: border-box;
+    }
+    :global(.bg) {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+    .navbar {
+        display: flex;
+        width: 100%;
+        height: 50px;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: rgb(25, 13, 36);
+        border-bottom: 8px solid rgb(35, 23, 46);
+        overflow: hidden;
+        z-index: 1000;
+    }
+    .navbuttons {
+        position: relative;
+        width: auto;
+        height: 25px;
+        margin: auto;
+        overflow: hidden;
+        /*background-color: white;*/
+    }
+    .navbutton {
+        margin-left: 16px;
+        margin-right: 16px;
+        font-size: 25px;
+        text-decoration: none;
+        color: rgb(180, 180, 180);
+    }
+</style>
+
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>test</h1>
-<p>sigma</p>
+<script lang="ts">
+    let canvas: HTMLCanvasElement;
+    let circles = $state(<number[][]>[]);
+    const TOPBARHEIGHT = 50;
+
+    function generateCircle(x: number, y: number) {
+        return [
+            255,          // color
+            x,            // x
+            y,            // y
+            4,            // radius
+            0,            // start angle
+            Math.PI*2,    // end angle
+            Math.random()*Math.PI*2, // direction
+            Math.random(), // magnitude
+            x,            // og x
+            y             // og y
+        ];
+    }
+
+    $effect(() => {
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        const context = canvas.getContext('2d')!;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Render circles
+        for (let i=0; i<circles.length; i++) {
+            let c = circles[i];
+            for (let n=i+1; n<circles.length; n++) {
+                let c2 = circles[n];
+                let distsqr = (c[1]-c2[1])*(c[1]-c2[1])+(c[2]-c2[2])*(c[2]-c2[2]);
+                if (distsqr < 26000) {
+                    let b = Math.min((26000-distsqr)*0.01, 255);
+                    context.strokeStyle = "rgba(" + b + ", " + b + ", " + b + ", " + b + ")";
+                    context.beginPath();
+                    context.moveTo(c[1], c[2]);
+                    context.lineTo(c2[1], c2[2]);
+                    context.stroke();
+                }
+            }
+        }
+        circles.forEach(c => {
+            context.fillStyle = "#" + c[0].toString(16) + c[0].toString(16) + c[0].toString(16) + "ff";
+            context.beginPath();
+            context.arc(c[1], c[2], c[3], c[4], c[5]);
+            context.fill();
+        });
+
+        // Init circles
+        if (circles.length == 0) {
+            const W = canvas.width/2;
+            const H = (canvas.height-TOPBARHEIGHT)/2;
+            const R = Math.min(W, H);
+            const N = 16;
+            for (let i=0; i<N; i++) {
+                circles.push(generateCircle(W + 0.75*R*Math.cos(2*Math.PI*i/N), TOPBARHEIGHT + H + 0.75*R*Math.sin(2*Math.PI*i/N)));
+            }
+            for (let i=0; i<N; i++) {
+                circles.push(generateCircle(W + 0.9*R*Math.cos(2*Math.PI*(i+0.5)/N), TOPBARHEIGHT + H + 0.9*R*Math.sin(2*Math.PI*(i+0.5)/N)));
+            }
+        }
+    });
+</script>
+
+<canvas bind:this={canvas} class="bg" id="bg"></canvas>
